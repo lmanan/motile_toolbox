9d661b286abc5db9404ad05a59b3c94471b9b00e
lmanan <manan.lalit@gmail.com>
2024-09-10 15:20:21 -0400

    Allow handling hyper-edges.

--- src/motile_toolbox/candidate_graph/graph_to_nx.py
+++ src/motile_toolbox/candidate_graph/graph_to_nx.py
@@ -14,8 +14,14 @@ def graph_to_nx(graph: TrackGraph) -> nx.DiGraph:
     nx_graph = nx.DiGraph()
     nodes_list = list(graph.nodes.items())
     nx_graph.add_nodes_from(nodes_list)
-    edges_list = [
-        (edge_id[0], edge_id[1], data) for edge_id, data in graph.edges.items()
-    ]
+    edges_list = []
+    for edge_id, _ in graph.edges.items():
+        if type(edge_id[0]) is tuple:
+            (a,) = edge_id[0]
+            b, c = edge_id[1]
+            edges_list.append((a, b))
+            edges_list.append((a, c))
+        else:
+            edges_list.append((edge_id[0], edge_id[1]))
     nx_graph.add_edges_from(edges_list)
     return nx_graph
